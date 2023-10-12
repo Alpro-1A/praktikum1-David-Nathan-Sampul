6ede4272a0a0fbebdc17eb648b47c4f1455ac092
nate <evannathan78@gmail.com>
2023-10-12 17:38:04 +0700

    adding file praktikum.cpp

---
+++ praktikum.cpp
@@ -0,0 +1,25 @@
+#include <iostream> // input output stream
+
+using namespace std; 
+
+int main() {
+  string namaSaya;
+  namaSaya = "David Nathan Sampul";
+  string kelasSaya;
+  kelasSaya = "1A - Informatika";
+  string npmSaya;
+  npmSaya = "2310631170157";
+  string citacitaSaya;
+  citacitaSaya = "Programmer";
+  string MotivasiSaya;
+  MotivasiSaya = "Ingin memperluas ilmu pengetahuan coding di dalam kampus dan menambah relasi sebanyak-banyaknya.";
+
+  cout << "---------------------------------------------------------------------------------------------------------------------------" << endl;
+  cout << "Nama: " << namaSaya << endl << endl; // c output
+  cout << "NPM: " << npmSaya << endl << endl; 
+  cout << "Kelas: " << kelasSaya << endl << endl; 
+  cout << "Cita - Cita : " << citacitaSaya << endl << endl; 
+  cout << "Motivasi Masuk Informatika: " << MotivasiSaya << endl; 
+  cout << "---------------------------------------------------------------------------------------------------------------------------" << endl;
+  return 0;
+}
